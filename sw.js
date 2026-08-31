faa9675c7f0f3bd6d479a32f24d6be4232fc6a0d
Mesho.M.M <mshaer500000@gmail.com>
2026-08-31 05:25:23 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 const CACHE="kg-child-observer-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const ASSETS=["./","./index.html","./child-card.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{
